9eb5d72508969b3a9c588fc4aac959e5bf2866f3
José Marques Cardoso Souza <jmarques@crmma.local>
2024-12-16 13:41:28 -0300

    ajuste final do ParserTest

--- src/test/java/br/ufma/ecp/ParserTest.java
+++ src/test/java/br/ufma/ecp/ParserTest.java
@@ -4,10 +4,12 @@ import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
 
-// import java.io.IOException;
+
+import java.io.IOException;
 
 import org.junit.Test;
-// import org.junit.experimental.theories.suppliers.TestedOn;
+
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 public class ParserTest extends TestSupport {
 
@@ -313,6 +315,8 @@ public class ParserTest extends TestSupport {
       result = result.replaceAll("\r", ""); // no codigo em linux não tem o retorno de carro
       assertEquals(expectedResult, result);
   }
+
+
   @Test
   public void testParseIf() {
       var input = "if (direction = 1) { do square.moveUp(); }";
@@ -347,6 +351,7 @@ public class ParserTest extends TestSupport {
           <symbol> } </symbol>
         </ifStatement>
               """;
+
       var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
       parser.parseIf();
       var result = parser.XMLOutput();
@@ -354,11 +359,13 @@ public class ParserTest extends TestSupport {
       result = result.replaceAll("\r", ""); // no codigo em linux não tem o retorno de carro
       assertEquals(expectedResult, result);
   }
+
   @Test
   public void testParseDo() {
       var input = "do Sys.wait(5);";
       var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
       parser.parseDo();
+
       var expectedResult = """
           <doStatement>
           <keyword> do </keyword>
@@ -383,31 +390,16 @@ public class ParserTest extends TestSupport {
       assertEquals(expectedResult, result);
   }
 
+
+
+
+
   
    @Test
    public void testParserWithLessSquareGame() throws IOException {
        var input = fromFile("ExpressionLessSquare/SquareGame.jack");
        var expectedResult =  fromFile("ExpressionLessSquare/SquareGame.xml");
-       var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
-       parser.parse();
-       var result = parser.XMLOutput();
-       expectedResult = expectedResult.replaceAll("  ", "");
-       assertEquals(expectedResult, result);
-   }
-   @Test
-   public void testParserWithSquareGame() throws IOException {
-       var input = fromFile("Square/SquareGame.jack");
-       var expectedResult =  fromFile("Square/SquareGame.xml");
-       var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
-       parser.parse();
-       var result = parser.XMLOutput();
-       expectedResult = expectedResult.replaceAll("  ", "");
-       assertEquals(expectedResult, result);
-   }
-   @Test
-   public void testParserWithSquare() throws IOException {
-       var input = fromFile("Square/Square.jack");
-       var expectedResult =  fromFile("Square/Square.xml");
+
        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
        parser.parse();
        var result = parser.XMLOutput();
@@ -415,4 +407,30 @@ public class ParserTest extends TestSupport {
        assertEquals(expectedResult, result);
    }
 
+   @Test
+   public void testParserWithSquareGame() throws IOException {
+       var input = fromFile("Square/SquareGame.jack");
+       var expectedResult =  fromFile("Square/SquareGame.xml");
+
+       var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+       parser.parse();
+       var result = parser.XMLOutput();
+       expectedResult = expectedResult.replaceAll("  ", "");
+       assertEquals(expectedResult, result);
+   }
+
+
+   @Test
+   public void testParserWithSquare() throws IOException {
+       var input = fromFile("Square/Square.jack");
+       var expectedResult =  fromFile("Square/Square.xml");
+
+       var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+       parser.parse();
+       var result = parser.XMLOutput();
+       expectedResult = expectedResult.replaceAll("  ", "");
+       assertEquals(expectedResult, result);
+   }
+
+   
 }
